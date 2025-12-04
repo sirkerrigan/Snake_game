7179102676ff107828e23b3a6933e8a5e200e274
Nail Bildanov <nailbildanov@gmail.com>
2025-12-04 16:21:10 +0100

    added high score

--- scoreboard.py
+++ scoreboard.py
@@ -6,6 +6,8 @@ class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("data.txt") as data:
+            self.highscore = int(data.read())
         self.color("white")
         self.penup()
         self.goto(0, 265)
@@ -13,13 +15,21 @@ class ScoreBoard(Turtle):
         self.updateScoreboard()
         
     def updateScoreboard(self):
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.highscore}", align=ALIGNMENT, font=FONT)
 
-    def gameOver(self):
-        self.goto(0,0)
-        self.write("GAME OVER" , align= ALIGNMENT, font= FONT)
+    def reset(self):
+        if self.score > self.highscore:
+            self.highscore = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.highscore}") 
+        self.score = 0
+        self.updateScoreboard()
+
+    # def gameOver(self):
+    #     self.goto(0,0)
+    #     self.write("GAME OVER" , align= ALIGNMENT, font= FONT)
 
     def increaseScore(self):
         self.score += 1
-        self.clear()
         self.updateScoreboard()
--- snake.py
+++ snake.py
@@ -26,6 +26,13 @@ class Snake:
         new_part.goto(pos)
         self.segments.append(new_part)
 
+    def reset(self):
+        for seg in self.segments:
+            seg.goto(1000,1000)
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
+
 
     def extend(self):
         self.addSegment(self.segments[-1].pos())
